1a8cdee90536e24e06f86ac05c1fb820fbe76161
duor <mike.conor.murray+git_duor@gmail.com>
2025-03-27 16:50:27 +0000

    add playing feedback

--- site.js
+++ site.js
@@ -1,14 +1,37 @@
 let volume = 0.5;
 let audio;
+let playing;
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    const addProgress = (id, progress) => {
+        document.getElementById(id).style.background = `linear-gradient(90deg, hsl(305.5799999999999 37.39% 22.55%) ${progress}%,hsl(305 37.5% 12.55%) 100%)`;
+    }
+
+    const clearProgres = (id) => {
+        document.getElementById(id).style = "";
+    }
+
     const buttons = document.querySelectorAll('button');
     buttons.forEach(button => {
         button.addEventListener('click', (ev) => {
             const track = ev.target.id;
+            if (playing) {
+                clearProgres(playing);
+            }
+            playing = track;
             if (audio) { audio.pause(); }
             audio = new Audio(`./clips/${track}.mp3`);
             audio.volume = volume;
+
+            audio.addEventListener("ended", () => {
+                clearProgres(track);
+            });
+
+            audio.addEventListener("timeupdate", () => {
+                addProgress(track, (audio.currentTime / audio.duration) * 100);
+            });
+
             audio.play();
         });
     });
